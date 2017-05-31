5fab50e4e157aef587c54139f867ce5841714291
Donrockz <rockstarrockey18@gmail.com>
2017-05-31 23:12:03 +0530

    Add files via upload

---
+++ Assignment1.2/src/assignment2/ass2.java
@@ -0,0 +1,30 @@
+package assignment2;
+
+public class ass2 {
+
+	public static void main(String[] args) {
+		
+		int a = 20;
+		int b = a-- - --a;
+		
+		
+		int c = a--;
+		int d = a>>2;
+		int e = a&b;
+		
+		System.out.println("THe reult of first expression is " + b);
+		
+		System.out.println("THe reult of second expression is " + c);
+		
+		
+		
+		System.out.println("THe reult of third expression is " + d);
+		
+		
+		
+		System.out.println("THe reult of fourth expression is " + e);
+		
+		
+				
+	}
+}
